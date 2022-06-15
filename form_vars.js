770003a02b245397d57f78ba0210561b5ca3817a
Javier <jrey1977@yahoo.es>
2022-06-15 23:21:47 +0200

    Form vars finalizadfo

--- form_vars.js
+++ form_vars.js
@@ -12,30 +12,36 @@ http.createServer((req, res) => {
     fs.readFile("./form.html", (err, html) => {
         let html_string = html.toString();
         let arrayParams = [];
-        let params = {};
+        let objetoParams = {};
         let arrayConcidencias = html_string.match(/[^\{\}]+(?=\})/g);
+        console.log("arrayConcidencias es", arrayConcidencias);
         let nombre = "";
         if (req.url.indexOf("?") > 0) {
             // /?nombre=javi&puesto=siempre
             let url_data = req.url.split("?");
-            let arrayParams = url_data[1].split("&");
+            arrayParams = url_data[1].split("&");
             // [nombre=javi,puesto=siempre]
         }
 
         for (let i = arrayParams.length - 1; i >= 0; i--) {
             var param = arrayParams[i];
+            console.log("param es", param);
             // nombre=javi
             var param_dato = param.split("=");
             // [nombre,javi]
-            params[param_dato[0]] = [param_dato[1]];
+            objetoParams[param_dato[0]] = param_dato[1];
             // {nombre: javi}
         }
 
         for (let i = arrayConcidencias.length; i >= 0; i--) {
-            let valorVariable = eval(arrayConcidencias[i]);
+            let valorVariable = arrayConcidencias[i];
+            console.log(
+                valorVariable + " se reemplaza con ",
+                +objetoParams[valorVariable]
+            );
             html_string = html_string.replace(
-                "{" + arrayConcidencias[i] + "}",
-                valorVariable
+                "{" + valorVariable + "}",
+                objetoParams[valorVariable]
             );
         }
 
